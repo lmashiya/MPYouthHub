156a951991bae24fc1372d4e34a63bef67179d86
Lehlohonolo Mashiyane <lehlohonolo.mashiyane@eblocks.co.za>
2021-02-09 18:32:04 +0200

    Added images and logo...TODO fix logo

--- MPYouthHubMVP/MPYouthHubMVP/App_Start/BundleConfig.cs
+++ MPYouthHubMVP/MPYouthHubMVP/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace MPYouthHubMVP
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.jcarousel.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
